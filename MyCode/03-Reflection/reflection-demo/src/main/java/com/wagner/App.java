bef161c741701490367f50ca0930af19f8470c2a
Wagner Xu <wagnerhsu@hotmail.com>
2023-02-18 18:43:10 +0800

    feat: reflection-demo

---
+++ MyCode/03-Reflection/reflection-demo/src/main/java/com/wagner/App.java
@@ -0,0 +1,17 @@
+package com.wagner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Hello World!");
+    }
+}
