bea377f5f9b32741efb12c924c0e1e709cbc1c8b
Wagner Xu <wagnerhsu@hotmail.com>
2023-04-08 21:24:12 +0800

    test: change junit to jupiter api

--- MyCode/05-Lambda/lambda-demo/src/main/java/com/wagner/App.java
+++ MyCode/05-Lambda/lambda-demo/src/main/java/com/wagner/App.java
@@ -1,13 +1,21 @@
 package com.wagner;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Function;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        Function<Integer, Integer> func1 = App::AddOne;
+        assert func1.apply(2) == 3;
+        log.info("Hello");
+    }
+
+    public static int AddOne(int i) {
+        return i + 1;
     }
 }
