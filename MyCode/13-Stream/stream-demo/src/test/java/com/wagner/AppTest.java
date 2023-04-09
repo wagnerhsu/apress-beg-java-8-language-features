467fdac613f76f8a0a919f7d5eca872878dd2c5d
Wagner Xu <wagnerhsu@hotmail.com>
2023-04-09 23:12:33 +0800

    test: upgrade to junit 5.x

--- MyCode/13-Stream/stream-demo/src/test/java/com/wagner/AppTest.java
+++ MyCode/13-Stream/stream-demo/src/test/java/com/wagner/AppTest.java
@@ -1,38 +1,39 @@
 package com.wagner;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import lombok.var;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        assertTrue(true);
     }
+
+    @Test
+    public void reduceTest() {
+        var numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = numbers.stream()
+                .filter(n -> n % 2 == 1)
+                .map(n -> n * n)
+                .reduce(0, Integer::sum);
+        assertTrue(sum == 35);
+        sum = numbers.stream()
+                .filter(n -> n % 2 == 1)
+                .map(n -> n * n)
+                .reduce(0, (n1, n2) -> Integer.sum(n1, n2));
+        assertTrue(sum == 35);
+    }
+
 }
